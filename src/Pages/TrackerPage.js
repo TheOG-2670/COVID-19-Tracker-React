8accd6cc55ae9153eec4a4e5504fba7dcbef8e4f
TheOG-2670 <ogolan3589@gmail.com>
2021-06-10 03:39:55 +0000

    updated search bar; added global and country class states;updated fetch requests; temporarily disabled cache update interval

--- src/Pages/TrackerPage.js
+++ src/Pages/TrackerPage.js
@@ -9,27 +9,31 @@ export default class TrackerPage extends Component
     {
       super(props)
         this.state={
-          data:[],
+          global:null,
+          country:null,
           updateInterval:null
         }
       this.getCachedData=this.getCachedData.bind(this)
       this.handleSubmit=this.handleSubmit.bind(this)
+
+      //used to initialize cache for each country
+      // fetch('/api/covid/initData')
+      // .then(res=>{
+      //   return res.json()
+      // })
+      // .then(data=>{
+      //   console.log(data)
+      // })
     }
 
   getCachedData()
   {
-    fetch('/api/covid/cachedData')
+    fetch('/api/covid/global')
     .then(res=>{return res.json()})
     .then(data=>{
-      var cards=[]
-      for(var i=0; i < data.length; i++)
-      {
-          cards.push(
-            <CardContainer id={i} regionData={data[i]}/>
-          )
-      }
+      // console.log(data)
       this.setState({
-        data:cards
+        global: <CardContainer regionData={data}/>
       })
     })
   
@@ -40,24 +44,24 @@ export default class TrackerPage extends Component
     console.log("page loaded!")
     this.getCachedData()
     
-    var intervalID = setInterval(()=>{
-        this.getCachedData()
-        console.log("page updated!")
-    },3600000) //fetch data from api every hour starting at app launch
+    // var intervalID = setInterval(()=>{
+    //     this.getCachedData()
+    //     console.log("page updated!")
+    // },3600000) //fetch data from api every hour starting at app launch
 
-    this.setState({
-      updateInterval: intervalID
-    })
+    // this.setState({
+    //   updateInterval: intervalID
+    // })
   }
 
-  componentWillUnmount()
-  {
-    clearInterval(this.state.updateInterval)
-  }
+  // componentWillUnmount()
+  // {
+  //   clearInterval(this.state.updateInterval)
+  // }
 
   handleSubmit(countryName) 
   {
-    console.log(countryName)
+    // console.log(countryName)
 
     fetch('/api/covid/countries/' + countryName)
       .then(res => {
@@ -65,16 +69,46 @@ export default class TrackerPage extends Component
       })
       .then(data => {
         console.log(data)
+
+        this.setState({
+          country: <CardContainer id={data.id} regionData={data}/>
+        })
       })
   }
 
     render()
     {
+      const countryPlaceholderStyle={
+        marginTop:'250px', 
+        color:'grey', 
+        border:'3px dashed grey', 
+        borderRadius:'10px', 
+        fontSize:'20px', 
+        textAlign:'center', 
+        opacity:'0.5'
+      }
         return(
-          <div>
+          <div className='container'>
             <SearchBar onClick={this.handleSubmit}/>
-              <div className="col-md-6 offset-md-3">
-                {this.state.data[0]}
+              <div className="row">
+                <div className='col-md-5'>
+                  {this.state.global}
+                </div>
+                <div className='col-md-2 d-flex justify-content-center'>
+                  <span style={{display: 'inline-block', marginTop:'30px', height:'430px', width:'3px', backgroundColor:'lightgrey'}}></span>
+                </div>
+
+                <div className='col-md-5'>
+                  {
+                    this.state.country!==null
+                    ?
+                    this.state.country
+                    :
+                    <div className='d-flex justify-content-center' 
+                        style={countryPlaceholderStyle}
+                    >Searched country will appear here</div>
+                  }
+                </div>
               </div>
           </div>
         )
